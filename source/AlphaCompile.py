4ae8cd02f67ee9ec21ab48acc1bfb75ac3801463
Tom <thoward12@outlook.com>
2019-05-02 19:04:04 -0400

    AlphaCompile is training!

--- source/AlphaCompile.py
+++ source/AlphaCompile.py
@@ -8,20 +8,20 @@ STEPS = 10
 
 
 class AlphaCompile(AlphaZero):
-    def __init__(self, input_dim: int = 5, output_dim: int = len(FLAGS) + 1):
+    def __init__(self, input_dim: int, output_dim: int = len(FLAGS) + 1):
         self.hidden = 5
         body = nn.Sequential(
             nn.Linear(input_dim, self.hidden),
-            nn.Dropout(0.4),
+            nn.Dropout(0.5),
             nn.Linear(self.hidden, self.hidden),
         )
         super().__init__(input_dim, output_dim, body)
 
 
 if __name__ == "__main__":
-    model = AlphaCompile()
     programs = Programs()
     train, test = programs.filter(programs[0])
+    model = AlphaCompile(train[0].observation_space, len(FLAGS) + 1)
     for program in train:
         model.train()
         model.play(program)
