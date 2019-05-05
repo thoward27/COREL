e24abdc9565bd1b8cd05fbbc6d8a02ffa259993a
Tom <thoward12@outlook.com>
2019-05-05 17:38:06 -0400

    Training successfully!

--- source/AlphaCompile.py
+++ source/AlphaCompile.py
@@ -1,34 +1,94 @@
-from torch import nn
+from random import shuffle
 
 from Benchmarks import Programs
-from PyTorchRL.agents.AlphaZero import AlphaZero
+from PyTorchRL.agents.AlphaZero import *
+from source.MCTS import mcts
 from source.config import FLAGS
 
 STEPS = 10
 
 
-class AlphaCompile(AlphaZero):
+class AlphaCompile(nn.Module):
     def __init__(self, input_dim: int, output_dim: int = len(FLAGS) + 1):
-        self.hidden = 5
-        body = nn.Sequential(
-            nn.Linear(input_dim, self.hidden),
+        super().__init__()
+
+        hidden = (input_dim + output_dim) // 2
+        self.body = nn.Sequential(
+            nn.Linear(input_dim, hidden),
+            nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(self.hidden, self.hidden),
+            nn.Linear(hidden, hidden),
+            nn.ReLU()
         )
-        super().__init__(input_dim, output_dim, body)
+        self.actions = nn.Linear(hidden, output_dim)
+        self.value = nn.Linear(hidden, 1)
+
+        self.optim = optim.Adam(self.parameters())
+        self.loss_value = nn.MSELoss()
+        self.loss_policy = nn.BCEWithLogitsLoss()
+        return
+
+    def forward(self, x):
+        if isinstance(x, np.ndarray):
+            x = torch.from_numpy(x).float()
+        h = self.body(functional.softmax(x, dim=0))
+        return functional.softmax(self.actions(h), dim=0), tanh(self.value(h))
+
+    def play(self, game, episodes=10, steps=10, render=False):
+        for e in range(episodes):
+            logging.debug("Episode {}: {}".format(e, game))
+            state = game.reset()
+            for s in range(steps):
+                if render:
+                    game.render()
+
+                if self.training:
+                    # Policy Update
+                    pi = mcts(self, state, deepcopy(game), simulations=25)
+                    policy, v = self.forward(state)
+                    loss_policy = self.loss_policy(policy, functional.softmax(torch.tensor([n.w for n in pi]), dim=0))
+                    logging.debug("{} -> w():{}".format([round(n.item(), 2) for n in policy], [round(n.w, 2) for n in pi]))
+                    logging.debug("{} -> pr():{}".format([round(n.item(), 2) for n in policy], [round(n.pr(), 2) for n in pi]))
+                    logging.debug([round(n.item(), 2) for n in self.forward(state)[0]])
+
+                    # Value Update
+                    state, rew, done, info = game.step(max(pi).action)
+                    loss_value = self.loss_value(v, torch.tensor(rew))
+
+                    logging.debug("Action {} got reward {}".format(max(pi).action, round(rew, 2)))
+
+                    loss = loss_policy + loss_value
+                    self.optim.zero_grad()
+                    loss.backward()
+                    self.optim.step()
+
+                    # MLFlow
+                    mlflow.log_metric("Loss", (loss_policy + loss_value).item())
+                    mlflow.log_metric("Training reward", rew)
+                    mlflow.log_metric("Training Value Error", loss_value.item())
+                    mlflow.log_metric("Training Policy Error", loss_policy.item())
+                else:
+                    pi, z = self.forward(state)
+                    state, rew, done, info = game.step(torch.argmax(pi).item())
+
+                    mlflow.log_metric("Testing reward", rew)
+                if done:
+                    break
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     programs = Programs()
     train, test = programs.filter(programs[0])
     model = AlphaCompile(train[0].observation_space, len(FLAGS) + 1)
+    logging.info(model)
+    shuffle(train)
     for program in train:
+        logging.info("Training on {}".format(repr(program)))
         model.train()
         model.play(program)
 
-        model.eval()
-        model.play(program)
-
     for program in test:
+        logging.info("Testing on {}".format(repr(program)))
         model.eval()
         model.play(program)
---
+++ source/MCTS.py
@@ -0,0 +1,98 @@
+import logging
+from copy import deepcopy
+from functools import total_ordering
+
+from math import log, sqrt
+from torch import nn, Tensor
+from torch.nn.functional import softmax
+
+from source.config import FLAGS
+
+c_base = 0.2
+c_init = 0.5
+
+
+@total_ordering
+class Node:
+    def __init__(self, state: Tensor, action: int, prior: float, parent=None):
+        self.state: Tensor = state
+        self.action: int = action
+        self.parent: Node = parent
+        self.done: bool = False
+
+        self.children: list = []
+        self.n: int = 0
+        self.w: float = 0
+        self.p: float = prior
+
+    def __repr__(self) -> str:
+        return "({}, {})".format(self.action, round(self.w, 2))
+
+    def __eq__(self, other) -> bool:
+        return self.q() + self.u() == other.q() + other.u()
+
+    def __gt__(self, other) -> bool:
+        return self.q() + self.u() > other.q() + other.u()
+
+    def pr(self) -> float:
+        return self.q() + self.u()
+
+    def q(self) -> float:
+        try:
+            return self.w / self.n
+        except ZeroDivisionError:
+            return 0
+
+    def c(self) -> float:
+        try:
+            return (log((1 + self.parent.n + c_base) / c_base)) + c_init
+        except AttributeError:
+            return c_init
+
+    def u(self) -> float:
+        try:
+            return (self.c() * self.p * sqrt(self.parent.n)) / (1 + self.n)
+        except AttributeError:
+            return self.p / (1 + self.n)
+
+
+def mcts(model: nn.Module, state: Tensor, game, simulations: int = 10):
+    # Start from current root
+    policy, value = model.forward(state)
+    logging.debug("MCTS Initial Policy: {}".format([round(p.item(), 2) for p in policy]))
+
+    tree = [Node(state, a, p.item()) for a, p in enumerate(softmax(policy, dim=0))]
+
+    # Traverse to leaf
+    for s in range(simulations):
+        logging.debug("MCTS # {}, Tree: {}".format(s, [round(t.pr(), 2) for t in tree]))
+        node = max(tree)
+        flags = [node.action]
+        while node.children:
+            node = max(node.children)
+            flags.append(node.action)
+
+        # The agent has selected "pass"
+        if flags[-1] == len(FLAGS):
+            node.done = True
+
+        if not node.done:
+            simulation = deepcopy(game)
+            # Expand leaf
+            state, value, done, info = simulation.step(' '.join([FLAGS[f] for f in flags]))
+            node.done = done
+            logging.debug("Compiled with flags {}, got reward {}".format(flags, round(value, 2)))
+
+            policy, _ = model.forward(state)
+            node.children = [Node(state, a, p.item(), parent=node) for a, p in enumerate(policy)]
+        else:
+            value = node.w
+
+        # Backward step
+        while node:
+            node.n += 1
+            node.w += value
+            node = node.parent
+
+    # return the new policy
+    return tree
